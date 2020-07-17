abf3e2ff3d59384abcbecd4e2ae6ba60c889b1f4
Alvin <fulmar38251@letterboxes.org>
2020-07-16 21:14:47 -0400

    Create calculateTotal function using map

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
     Do work here! 
     Use the price, the quantity and the discount to build your calculation
 */
+
+function calculateTotal (items) {
+  return items.map(function (item) {
+    let newTotal = item.price * (1 - item.discount) * item.quantity
+
+    item.total = parseInt(newTotal * 100) / 100
+
+    return item
+  })
+}
+
+module.exports = calculateTotal
