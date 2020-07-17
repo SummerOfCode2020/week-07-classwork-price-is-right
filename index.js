0b0f1d3e2fd7a454adfced8aaa8f0f27f87aac71
Yor Name <hollisjamison@gmail.com>
2020-07-16 20:46:27 -0400

    Implements calcualteTotal function which calculates the total for an array of items

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
     Do work here! 
     Use the price, the quantity and the discount to build your calculation
 */
+function calculateTotal(inputArray) {
+  return inputArray.map(calculateIndividualTotal)
+}
+
+
+function calculateIndividualTotal(individualItem) {
+  let calculatedTotal = individualItem.price * (1 - individualItem.discount) * individualItem.quantity
+
+  individualItem.total = parseInt(calculatedTotal * 100) / 100
+
+  return individualItem
+}
+
+module.exports = calculateTotal
