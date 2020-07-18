d236f3ab5fc9c4371f43ad38139798f8208f581d
Your Name <pharrington375@gmail.com>
2020-07-18 13:27:10 -0400

    Init calculateTotal function and export to test file

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
     Do work here! 
     Use the price, the quantity and the discount to build your calculation
 */
+
+function calculateTotal(items) {
+    return items.map(calculateItemTotal)
+}
+
+function calculateItemTotal(item) {
+    const total = item.price * (1 - item.discount) * item.quantity
+
+    item.total = parseInt(total * 100) / 100
+
+    return item
+}
+
+module.exports = calculateTotal
